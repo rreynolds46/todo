2052803d08a2d67b91bd4e0238e9c3764a25f9b1
Arkos <reynro03@gmail.com>
2018-02-13 23:04:10 -0500

    Day 2 Updates 2
    - Added Hide Complete functionality
    - Added Functionality for Deletion
    - Added Functionality to Make Complete

--- Resources/JS/main.js
+++ Resources/JS/main.js
@@ -22,12 +22,14 @@ function todoAdd() {
     
     var remove = document.createElement("button");
     remove.setAttribute('class','todo-remove');
+    remove.onclick = todoRemove;
     var innerRemove = document.createElement("i");
     innerRemove.setAttribute('class','fas fa-trash-alt fa-2x')
     remove.appendChild(innerRemove);
 
     var complete = document.createElement("button");
     complete.setAttribute('class','todo-complete');
+    complete.onclick = todoComplete;
     var innerComplete = document.createElement("i");
     innerComplete.setAttribute('class','fas fa-check fa-2x')
     complete.appendChild(innerComplete)
@@ -42,10 +44,36 @@ function todoAdd() {
     document.getElementById("new-activity").value = '';
 }};
 
+//Show Remove and Complete Buttons on Hover
 function todoHover() {
     this.childNodes[1].style.visibility = "visible";
 };
 
 function todoHoverEnd() {
     this.childNodes[1].style.visibility = "hidden";
+}
+
+// Remove and Complete
+function todoRemove() {
+    this.parentNode.parentNode.remove();
+}
+
+function todoComplete() {
+    if(this.parentNode.parentNode.classList.contains("todo-active")) {
+        this.parentNode.parentNode.classList.remove("todo-active")
+    } else {
+        this.parentNode.parentNode.classList.add("todo-active")
+    }
+}
+
+// Hide Complete
+function todoHide() {
+    elements = document.getElementsByClassName("todo-active");
+    for(var i = 0; i < elements.length; i++) {
+        if(elements[i].classList.contains("todo-active-hidden")) {
+            elements[i].classList.remove("todo-active-hidden")
+        } else {
+            elements[i].classList.add("todo-active-hidden")
+        }
+    }
 }
